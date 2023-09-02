8a691347a0af691c25004e763f56c42e8c303b72
a2097855 <arushi.sadam@gmail.com>
2023-09-01 22:12:58 -0500

    adding commented exercise 6 for hw3

--- hw3/Exercise6_OddEvenVector/vectorOddEven.cpp
+++ hw3/Exercise6_OddEvenVector/vectorOddEven.cpp
@@ -1,54 +1,68 @@
+/*
+Name: Arushi Sadam
+UT EID: ars7724
+TACC username: a2097855
+*/
+
+/* Purpose: Seperate an input vector of integers into two construct vectors: one with all the odd integers of the input vector and one with all the even integers of the input vector. 
+*/
+
+//using standard input-output stream, vectors, and strings
 # include <iostream>
 # include <vector>
 # include <string>
-using std::vector;
-using std::string;
-using std::cout;
-using std::cin;
 
-void oddEvenFunc(vector <int> &inputV)
-{
+using std::vector;// using standard vectors
+using std::string;// using standard string function
+using std::cout; // using standard character output
+using std::cin;// using standard character input
+
+// sorting function that creates two vectors (one with odd ints and the other with even ints) out of the input vector
+void oddEvenFunc(vector <int> &inputV) 
+// &inputV = passing the input numbers vector by reference. Allows original vector in the main code to remain unchanged; this is not a necessary step for this program.
+
+{ //initializing vectors of integers for the odd numbers and for the even numbers
  vector<int>oddNum;
  vector<int>evenNum;
 
- for (int i = 0; i < inputV.size(); i++)
+ for (int i = 0; i < inputV.size(); i++) // loop that checks each number in the input vector; i represents the index in the input vector
    {
-     if (inputV[i] % 2 == 0)
+     if (inputV[i] % 2 == 0) // if the integer value at index i is even
        {
-  	evenNum.push_back(inputV[i]);
+	 evenNum.push_back(inputV[i]); // add the even integer to the even numbers vector
        }
-     else
+     else // integer value at index i is odd
        {
-        oddNum.push_back(inputV[i]);
+	 oddNum.push_back(inputV[i]); // add the odd integer to the odd numbers vector
        }
    }
- cout << "Odd numbers in input vector: ";
- for (int i = 0; i < oddNum.size(); i++)
+ cout << "Odd numbers in input vector: "; // output description for odd numbers vector
+ for (int i = 0; i < oddNum.size(); i++) // loops through each index (i) in odd numbers vector; starts with the first index of the vector
    {
-     cout << oddNum[i];
-     if (i != oddNum.size()-1)
-       cout << ", ";
+     cout << oddNum[i]; // output the value in the odd numbers vector corresponding with the index
+     if (i != oddNum.size()-1) //as long as not at the last number/value of the vector
+       cout << ", "; // add comma (between subsequent values of the odd number vector output to screen)
    }
- cout << "\nEven numbers in input vector: ";
- for (int i =0; i < evenNum.size(); i++)
+ cout << "\nEven numbers in input vector: "; // output description for even numbers vector
+ for (int i =0; i < evenNum.size(); i++) // loops through each index (i) in even numbers vector; starts with the first index of the vector
    {
-     cout << evenNum[i];
-     if (i != evenNum.size()-1)
-       cout << ", ";
+     cout << evenNum[i]; // output the value in the even numbers vector corresponding with the index
+     if (i != evenNum.size()-1) // as long as not at the last number/value of the vector
+       cout << ", "; // add comma (between subsequent values of the even number vector output to screen)
    }
- cout << "\n";
+ cout << "\n"; // add a new line
 }
 
 
-int main()
+int main() //main function that initializes the input vector and calls the function that sorts the numbers into odd or even groups.
 {
-   vector <int> inputV; 
-   cout << "Enter numbers as the input vector. Seperate each number with an ENTER. To stop entry, press a non-integer key. \n";
-   int inputNum;
-   while (cin >> inputNum)
+  vector <int> inputV; // initialize vector of input integers
+  cout << "Enter numbers as the input vector. Seperate each number with an ENTER. To stop entry, press a non-integer key. \n"; // instruction sentence, output
+  int inputNum; // initialize variable for input numbers
+  while (cin >> inputNum) // while input numbers are provided, remain in this loop and append to vector of input integers
    {
-   inputV.push_back(inputNum);
+     inputV.push_back(inputNum); // add each input number to vector of input integers
    }
 
-   oddEvenFunc(inputV);
+  oddEvenFunc(inputV); //call/apply the odd/even sorting function to the input number vector 
 }
