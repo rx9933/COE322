132e4fa826c19752543a88abd84699789bcfd7fb
rx9933 <arushi.sadam@gmail.com>
2023-09-09 08:35:07 -0500

    updating goldbach

--- hw4/goldbach.cpp
+++ hw4/goldbach.cpp
@@ -4,29 +4,6 @@ using std::vector;
 using std::cout;
 using std::cin;
 
-int generateP(int e, int bounds)
-{
-  vector <int> primes;
-  int currentNum = 2;
-  while (currentNum < e)
-    {
-      if (isprime(currentNum))
-	{
-	 primes.push_back;
-	}
-      currentNum ++;
-    }
-  pq(primes, e);
-  return primes; 
-}
-
-bool pq(vector <int> primes, int e)
-  for (int p::primes)
-  {
-    if isprime(e-p)
-       cout << "The number " << e << "is" << p << "+" << q;
-       break;
-  }
 
 bool isprime(int testNum)
 {
@@ -44,15 +21,44 @@ bool isprime(int testNum)
   return isNumPrime;
 }
 
+void pq(vector <int> primes, int e)
+  {
+  for (int p:primes)
+    {
+    int q = e-p;
+    if (isprime(q))
+      {
+       cout << "The number " << e << " is " << p << " + " << q << "\n";
+       break;
+      }
+    }
+  }
+
+void generateP(int e, int bound)
+{
+  vector <int> primes;
+  int currentNum = 2;
+    {
+      if (isprime(currentNum))
+	{
+	 primes.push_back(currentNum);
+	}
+      currentNum ++;
+    }
+  pq(primes, e);
+  // return primes; 
+}
+
+
 int main()
 {
   int bound;
   cout << "Enter a bound for the max value of an even number: ";
   cin >> bound;
   
-  for (int e = 4; e <= bound; e ++2)
+  for (int e = 4; e <= bound; e+2)
   {
-    vector <int> primesM = generateP(e);
+    generateP(e, bound);
   }
 }
 
