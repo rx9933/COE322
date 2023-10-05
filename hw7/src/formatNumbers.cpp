66d2ae27863d60c2eacc3d3136e0e09af2e3f7ba
a2097855 <arushi.sadam@gmail.com>
2023-10-04 22:18:39 -0500

    new comments

--- hw7/src/formatNumbers.cpp
+++ hw7/src/formatNumbers.cpp
@@ -7,7 +7,6 @@
 //include standard input output streams (to output grid)
 #include <iostream>
 using std::cout;
-using std::ostream;
 
 // include string usage (each hex number is written in a string)
 #include <string>
@@ -19,7 +18,8 @@ using fmt::format,fmt::print;
 
 // convert the input integer to a 2-digit hexadecimal string
 string to_hex(int number) {
-  return format("{:02x}", number); // (2-1) = 1 zero followed by lower case number (lower case x)
+  return format("{:02x}", number); // formats to 2 digit hexadecimal string. (x means hexadecimal)
+  //  return std::to_string(number);
 }
 
 // builds 16x16 grid of hex numbers 
@@ -27,8 +27,8 @@ int main()
 {
   for (int row = 0; row < 16; ++row) { // iterate over each row of grid
     for (int col = 0; col < 16; ++col) { // iterate over each column of grid
-      int number = row * 16 + col; // calculate number in base 10 based on position; row represents ones place, col represents tens place
-      string hex_number = to_hex(number); // convert base 10 number to hexadecimal number of type string
+      int number = row * 16 + col; // calculates base 10 value of hexidecimal number based on position; row represents ones place, col represents tens place
+      string hex_number = to_hex(number); // convert base 10 value to hexadecimal number of type string
       print("{:<3}", hex_number); // number of spaces between the start of each hex number (includes length of each hex number = 2 + 1 space = 3)
     }
     print("\n"); // add a new line after each row of numbers
